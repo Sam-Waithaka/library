860caded62b5790ca9fc3c03d46bc64a11a5e1d4
Sam <sw.waithakasamuel@gmail.com>
2025-01-07 13:42:50 +0300

    Add API tests for listing books
    - Created `APItests` class in `tests.py` to test the book list API endpoint.
    - Implemented `setUpTestData` to create a `Book` instance for testing.
    - Added `test_api_listview` to verify the API response status, book count, and content of the list view endpoint.

--- config/apis/tests.py
+++ config/apis/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from books.models import Book
+
+class APItests(APITestCase):
+    @classmethod
+
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title='The Great Gatsby', 
+            author='F. Scott Fitzgerald', 
+            subtitle='Gatsby the Greatest', 
+            isbn='978-3-16-148410-0',
+            )
+        
+    def test_api_listview(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
